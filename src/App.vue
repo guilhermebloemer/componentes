7e22519c23cb0de09733bc87e71c4035b3d42dea
guilhermebloemer <guilhermebloemers2@gmail.com>
2026-04-14 11:56:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue';
+import ButtonChil from './components/ButtonChil.vue';
+</script>
+
+<template>
+<div class="container">
+  <div>
+    <button class="btn btn-novo">Novo</button>
+    <button class="btn btn-editar">Editar</button>
+    <button class="btn btn-salvar">Salvar</button>
+    <button class="btn btn-cancelar">Cancelar</button>
+    <button class="btn btn-excluir">Excluir</button>
+
+  </div>
+  <div>
+    <ButtonChil :nome="'Novo'" :tipo="'btn'" :acao="'btn-novo'">
+
+    </ButtonChil>
+  </div>
+</div>
+</template>
+
+<style scoped>
+. {
+
+ justify-content: center;
+}
+.btn {
+  color: white;
+  background-color: gray;
+  border-radius: 5px;
+  padding: 10px 20px;
+  border: none;
+  cursor: crosshair;
+  margin: 1px;
+
+}
+
+.btn-novo {
+background-color: rgb(0, 223, 0);
+}
+.btn-editar {
+background-color: rgb(38, 128, 72);
+}
+.btn-salvar {
+background-color: rgb(221, 207, 6);
+}
+.btn-cancelar{
+background-color: rgb(235, 113, 0);
+}
+.btn-excluir {
+background-color: rgb(253, 8, 8);
+}
+
+
+</style>
---
+++ src/components/ButtonChil.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(['nome', 'tipo', 'acao'])
+</script>
+  <template>
+<button :class="[tipo, acao]">
+  {{ nome }}
+</button>
+  </template>
+
+
+<style scoped>
+.btn {
+  color: white;
+  background-color: gray;
+  border-radius: 5px;
+  padding: 10px 20px;
+  border: none;
+  cursor: crosshair;
+  margin: 1px;
+}
+
+.btn-novo {
+background-color: rgb(0, 223, 0);
+}
+
+</style>
